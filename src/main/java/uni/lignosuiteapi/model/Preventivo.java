9cd8284129220309d9af358d859c558ae9e3dbcd
Domenico Santagada <115139470+domenicosantagada@users.noreply.github.com>
2026-02-24 20:35:20 +0100

    Aggiunta p.iva nel db dei clienti

--- src/main/java/uni/lignosuiteapi/model/Preventivo.java
+++ src/main/java/uni/lignosuiteapi/model/Preventivo.java
@@ -15,10 +15,14 @@ public class Preventivo {
     private String invoiceNumber; // Collegato a invoiceNumber in Angular
     private Long utenteId;
     private String date;
+
     private String fromName;
     private String fromEmail;
+    private String fromPiva;
+
     private String toName;
     private String toEmail;
+    private String toPiva;
 
     @OneToMany(cascade = CascadeType.ALL)
     private List<PreventivoItem> items; // Le righe del preventivo
