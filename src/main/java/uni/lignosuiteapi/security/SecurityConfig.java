3e146d0cc48c99af6e38eef1ccc963cf74b662f8
Domenico Santagada <115139470+domenicosantagada@users.noreply.github.com>
2026-03-29 12:54:11 +0200

    Aggiunta configurazione di sicurezza e hashing delle password

--- src/main/java/uni/lignosuiteapi/security/SecurityConfig.java
+++ src/main/java/uni/lignosuiteapi/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,8 +22,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         http
-                .csrf(csrf -> csrf.disable())
+                // Disattiviamo CSRF
+                .csrf(AbstractHttpConfigurer::disable)
 
+                // Permettiamo tutto
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
